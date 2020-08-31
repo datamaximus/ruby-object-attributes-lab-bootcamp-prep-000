735f8ffec84f035e19f94c2c5bb54a9a9e3db050
Jason Wiram <jwiram@gmail.com>
2020-08-31 14:23:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,18 @@
 class Dog
+  
   def name=(name)
     @name = name
   end
   
   def name
     puts @name
+  end
+  
+  def breed=(breed)
+    @breed = breed
+  end
+  
+  def breed
+    puts breed
   end
 end
